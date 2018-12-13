5e929bc855dd6cd09a9b54b466b7301bd2d51848
Allard Hendriksen <allard.hendriksen@cwi.nl>
2018-12-13 16:47:39 +0100

    Remove spurious __init__ variables
    When you execute
    
    In [1]: import human_readable_project_name as he
    In [2]: help(he)
    
    The help page would previously show version_file and version_filename
    in the DATA section.
    
    Now, the version file reading is done in a private function and does
    not show up in the help page anymore.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -4,10 +4,17 @@
 
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
-import os.path
-version_filename = os.path.join(os.path.dirname(__file__), 'VERSION')
-with open(version_filename) as version_file:
-    __version__ = version_file.read().strip()
+
+
+def __get_version():
+    import os.path
+    version_filename = os.path.join(os.path.dirname(__file__), 'VERSION')
+    with open(version_filename) as version_file:
+        version = version_file.read().strip()
+    return version
+
+
+__version__ = __get_version()
 
 # Import all definitions from main module.
 from .{{ cookiecutter.project_slug }} import *
